dde8c2c39216faeaaab9a0a5d2fc554982126587
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-27 13:44:40 +0530

    Starter file with hero section completed

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import React from "react";
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-14",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
