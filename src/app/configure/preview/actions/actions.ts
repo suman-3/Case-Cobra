96a836db236550fbd361004c68f3a0529044cfea
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-28 20:22:26 +0530

    stripe session action created

--- src/app/configure/preview/actions/actions.ts
+++ src/app/configure/preview/actions/actions.ts
@@ -2,7 +2,9 @@
 
 import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
 import { db } from "@/db";
+import { stripe } from "@/lib/stripe";
 import { auth } from "@clerk/nextjs/server";
+import { Order } from "@prisma/client";
 
 export const createCheckoutSession = async ({
   configId,
@@ -33,5 +35,55 @@ export const createCheckoutSession = async ({
   if (material === "polycarbonate")
     price += PRODUCT_PRICES.material.polycarbonate;
 
-  
+  let order: Order | undefined = undefined;
+
+  const existingOrder = await db.order.findFirst({
+    where: {
+      userId: userId,
+      configurationId: configuration.id,
+    },
+  });
+
+  if (existingOrder) {
+    order = existingOrder;
+  } else {
+    order = await db.order.create({
+      data: {
+        amount: price / 100,
+        userId: userId,
+        configurationId: configuration.id,
+      },
+    });
+  }
+
+  const product = await stripe.products.create({
+    name: "Custom iPhone Case",
+    images: [configuration.imageUrl],
+    default_price_data: {
+      currency: "usd",
+      unit_amount: price,
+    },
+  });
+
+  const stripeSession = await stripe.checkout.sessions.create({
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
+    payment_method_types: ["link", "amazon_pay", "card", "link", "mobilepay"],
+    mode: "payment",
+    shipping_address_collection: {
+      allowed_countries: ["US", "CA", "IN"],
+    },
+    metadata: {
+      userId: userId,
+      orderId: order.id,
+    },
+    line_items: [
+      {
+        price: product.default_price as string,
+        quantity: 1,
+      },
+    ],
+  });
+
+  return { url: stripeSession.url };
 };
