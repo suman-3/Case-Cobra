bd0c6c6ac89823baa3256ad1abeea799c5dfb92e
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-28 12:17:05 +0530

    colour and model option added and array created of materials and finishes

--- src/validators/option-validator.ts
+++ src/validators/option-validator.ts
@@ -4,6 +4,8 @@
 // bg-green-800 border-green-800
 // bg-purple-900 border-purple-900
 
+import { PRODUCT_PRICES } from "@/config/products";
+
 export const COLORS = [
   {
     label: "Black",
@@ -31,3 +33,73 @@ export const COLORS = [
     tw: "purple-900",
   },
 ] as const;
+
+export const MODELS = {
+  name: "models",
+  options: [
+    {
+      label: "iPhone X",
+      value: "iphonex",
+    },
+    {
+      label: "iPhone 11",
+      value: "iphone11",
+    },
+    {
+      label: "iPhone 12",
+      value: "iphone12",
+    },
+    {
+      label: "iPhone 13",
+      value: "iphone13",
+    },
+    {
+      label: "iPhone 14",
+      value: "iphone14",
+    },
+    {
+      label: "iPhone 15",
+      value: "iphone15",
+    },
+    {
+      label: "iPhone 16",
+      value: "iphone16",
+    },
+  ],
+} as const;
+
+export const MATERIALS = {
+  name: "material",
+  options: [
+    {
+      label: "Silicone",
+      value: "silicone",
+      description: undefined,
+      price: PRODUCT_PRICES.material.silicone,
+    },
+    {
+      label: "Soft Polycarbonate",
+      value: "polycarbonate",
+      description: "Scratch-resistant coating",
+      price: PRODUCT_PRICES.material.polycarbonate,
+    },
+  ],
+} as const;
+
+export const FINISHES = {
+  name: "finish",
+  options: [
+    {
+      label: "Smooth Finish",
+      value: "smooth",
+      description: undefined,
+      price: PRODUCT_PRICES.finish.smooth,
+    },
+    {
+      label: "Textured Finish",
+      value: "textured",
+      description: "Soft grippy texture",
+      price: PRODUCT_PRICES.finish.textured,
+    },
+  ],
+} as const;
