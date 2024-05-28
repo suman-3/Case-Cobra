3420ad0257d48b19f0000eedae513ac502b415a8
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-28 10:44:01 +0530

    configuration design tab mostly completed

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,11 +1,38 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import React from "react";
+import DesignConfigurator from "../_components/DesignConfigurator";
 
-
-import React from 'react'
-
-const page = async() => {
-  return (
-    <div>page</div>
-  )
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
 }
 
-export default page
+const page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      imageUrl={imageUrl}
+      configId={configuration.id}
+      imageDimensions={{ width, height }}
+    />
+  );
+};
+
+export default page;
