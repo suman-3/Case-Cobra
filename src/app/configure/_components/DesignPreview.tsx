78b6c94ca395d1da08ac64b9f11f613f3d4dbda3
Suman Mondal <mondalsumanrock20@gmail.com>
2024-05-28 16:45:02 +0530

    checkout button add

--- src/app/configure/_components/DesignPreview.tsx
+++ src/app/configure/_components/DesignPreview.tsx
@@ -129,6 +129,12 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
                 </div>
               </div>
             </div>
+
+            <div className="mt-8 flex justify-end pb-12">
+              <Button className="px-4 sm:px-6 lg:px-8">
+                Check out <ArrowRight className="h-4 w-4 ml-1.5 inline" />
+              </Button>
+            </div>
           </div>
         </div>
       </div>
